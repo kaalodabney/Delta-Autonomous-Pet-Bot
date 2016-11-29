a422d42a55629480d8666d83f9f77a255b5b102b
kaalodabney <kaalodabney@gmail.com>
2016-11-28 21:28:36 -0800

    faces

--- RPI/ledManager.py
+++ RPI/ledManager.py
@@ -6,11 +6,102 @@ class LedManager:
     startTime = time.time()
     runningRoutine = 0
 
-    def update(self):
+    happyFace = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0]]
+
+    defaultFace = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0],
+    [0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    sadFace = [
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0],
+    [0,0,1,1,1,0,0,0,0,0,0,1,1,1,0,0],
+    [1,1,0,0,0,1,0,0,0,0,1,0,0,0,1,1],
+    [0,1,0,1,0,1,0,0,0,0,1,0,1,0,1,0],
+    [0,1,0,0,0,1,0,0,0,0,1,0,0,0,1,0],
+    [0,0,1,1,1,0,0,0,0,0,0,1,1,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    sadFace2 = [
+    [0,0,0,0,0,0,1,0,0,1,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,1,1,0,0,0,1,0,0,1,0,0,0,1,1,0],
+    [0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0],
+    [0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    sleepingFace4 = [
+    [0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0],
+    [0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0],
+    [0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0],
+    [0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0]]
+
+    tiredFace = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,0,1,1,1,1,1,0,0,1,1,1,1,1,0,0],
+    [0,0,1,1,1,1,1,0,0,1,1,1,1,1,0,0],
+    [0,0,1,0,1,0,1,0,0,1,0,1,0,1,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,1,1,1,0,0,0,0,1,1,1,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    hurtFace = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0],
+    [0,0,0,1,0,1,0,0,0,0,1,0,1,0,0,0],
+    [0,0,1,0,0,0,1,0,0,1,0,0,0,1,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+
+    hurtFace2 = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+    [0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0],
+    [0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0],
+    [0,0,1,1,1,1,0,0,0,0,1,1,1,1,0,0],
+    [0,0,1,0,0,1,0,0,0,0,1,0,0,1,0,0],
+    [0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]]
+    
+
+    def update(self, happyLevel, fatigueLevel, us, ir):
         if self.runningRoutine == 0:
             self.findRoutine()
-        elif self.runningRoutine == 1:
-            self.flash()
+        if self.runningRoutine == 1:
+            self.setHappyFace()
+        elif self.runningRoutine == 2:
+            self.setDefaultFace()
+        elif self.runningRoutine == 3:
+            self.setSadFace()
+        elif self.runningRoutine == 4:
+            self.setTiredFace()
+        elif self.runningRoutine == 5:
+            self.setSleepingFace()
+        elif self.runningRoutine == 6:
+            self.setHurtFace()
 
     def findRoutine(self):
         self.runningRoutine = 1
@@ -53,4 +144,38 @@ class LedManager:
                 self.device.pixel(x,y,0,redraw=False)
         self.device.flush()
         
+    def setHappyFace(self):
+        for x in range(16):
+            for y in range(8):
+                self.device.pixel(x,y,self.happyFace[y][x],redraw=False)
+        self.device.flush()
 
+    def setDefaultFace(self):
+        for x in range(16):
+            for y in range(8):
+                self.device.pixel(x,y,self.defaultFace[y][x],redraw=False)
+        self.device.flush()
+
+    def setSadFace(self):
+        for x in range(16):
+            for y in range(8):
+                self.device.pixel(x,y,self.sadFace2[y][x],redraw=False)
+        self.device.flush()
+
+    def setSleepingFace(self):
+        for x in range(16):
+            for y in range(8):
+                self.device.pixel(x,y,self.sleepingFace[y][x],redraw=False)
+        self.device.flush()
+
+    def setTiredFace(self):
+        for x in range(16):
+            for y in range(8):
+                self.device.pixel(x,y,self.tiredFace[y][x],redraw=False)
+        self.device.flush()
+
+    def setHurtFace(self):
+        for x in range(16):
+            for y in range(8):
+                self.device.pixel(x,y,self.hurtFace2[y][x],redraw=False)
+        self.device.flush()
